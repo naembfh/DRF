49fc9358d6d7fc37b20ac764e486efeefbb57a2c
Mehedi Nayem <naembfh@gmail.com>
2023-10-13 04:18:58 +0800

    cursor pagination

--- gs39/api/mypagination.py
+++ gs39/api/mypagination.py
@@ -4,4 +4,4 @@ class MyLimitOffsetPagination(LimitOffsetPagination):
     default_limit= 5
     
     
-    #http://127.0.0.1:8000/studentapi/?limit=6
+    #http://127.0.0.1:8000/studentapi/?limit=6&offset=8
